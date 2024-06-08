1dcc1349e57e8c30b6966a827971b4c14986000f
purpl3pineapple <opaloctopus1005@gmail.com>
2024-06-08 14:28:32 -0400

    improving submission functionality

--- submission.js
+++ submission.js
@@ -1,17 +1,27 @@
 /**
  * Source: {@link https://codepen.io/levinunnink-the-bashful/pen/YzxPyoG?editors=0010}
  */
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   const form = document.querySelector("form");
-  form.addEventListener("submit", function (e) {
+
+  form.addEventListener("submit", e => {
     e.preventDefault();
-    const data = new FormData(form);
     const action = e.target.action;
+    const body = new FormData(form);
+
     fetch(action, {
       method: "POST",
-      body: data,
-    }).then(() => {
-      alert("Success!");
-    });
+      cache: "no-cache",
+      mode: "cors",
+      body,
+    })
+      .then(async result => {
+        if (result.ok) {
+          alert("Your request was successfully submitted!");
+        } else {
+          throw new Error("Bad network response");
+        }
+      })
+      .catch(e => console.log({ POST_ERROR: e.message }));
   });
 });
