b34cb432c21f0476614a782c84935fcfc1a9a67e
purpl3pineapple <opaloctopus1005@gmail.com>
2024-06-11 01:12:20 -0400

    adding re-captcha for validation

--- submission.js
+++ submission.js
@@ -1,3 +1,27 @@
+let CAPTCHA = null;
+const captcha_success = document.getElementById("captcha-success");
+const captcha_failed = document.getElementById("captcha-failed");
+
+async function recaptcha_validated() {
+  CAPTCHA = true;
+  captcha_success.classList.remove("d-none");
+  captcha_failed.classList.add("d-none");
+  setTimeout(recaptcha_expired, 120_000);
+}
+
+function recaptcha_failed() {
+  CAPTCHA = false;
+  captcha_failed.innerHTML = "recaptcha failed";
+  captcha_failed.classList.remove("d-none");
+  captcha_success.classList.add("d-none");
+}
+
+function recaptcha_expired() {
+  CAPTCHA = null;
+  captcha_success.classList.add("d-none");
+  captcha_failed.classList.add("d-none");
+}
+
 /**
  * Source: {@link https://codepen.io/levinunnink-the-bashful/pen/YzxPyoG?editors=0010}
  */
@@ -6,6 +30,15 @@ window.addEventListener("load", () => {
 
   form.addEventListener("submit", e => {
     e.preventDefault();
+    if (CAPTCHA === null) {
+      captcha_failed.innerHTML = "Please complete the recaptcha.";
+      captcha_failed.classList.remove("d-none");
+
+      setTimeout(() => {
+        captcha_failed.classList.add("d-none");
+      }, 5000);
+      return;
+    }
     const action = e.target.action;
     const body = new FormData(form);
 
@@ -18,6 +51,7 @@ window.addEventListener("load", () => {
       .then(async result => {
         if (result.ok) {
           alert("Your request was successfully submitted!");
+          location.reload();
         } else {
           throw new Error("Bad network response");
         }
